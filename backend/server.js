9f55a9329d8cf9d35b6e6d051d254211c0e3777a
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2024-05-22 12:12:47 +0300

    working on the server

--- backend/server.js
+++ backend/server.js
@@ -137,11 +137,11 @@ app.post('/api/order', (req, res) => {
 
 // Define the port
 
-const PORT = process.env.PORT || 5000;
+
 const port = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port,  "0.0.0.0",() => {
+  console.log(`Server running on port ${port}`);
 });
 
 
