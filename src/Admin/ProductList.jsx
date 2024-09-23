50d89d7d276a9e4a956beb39588f74aa4e41cdb0
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2024-09-23 20:14:53 +0300

    worked on the cart css

--- src/Admin/ProductList.jsx
+++ src/Admin/ProductList.jsx
@@ -268,16 +268,9 @@ const ProductsList = () => {
       )}
 
       {loading ? (
-        <div className="dot-spinner">
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-          <div className="dot-spinner__dot"></div>
-        </div>
+         <div className="center-spinner">
+         <div className="user-spinner"></div>
+       </div>
       ) : (
         <ul>
           {filteredProducts.length > 0 ? (
